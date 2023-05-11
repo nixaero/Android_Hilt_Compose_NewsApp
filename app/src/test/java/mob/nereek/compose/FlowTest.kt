e42aa9a292e565d80b70af27bcb0233be7548761
nixaero <43453774+nixaero@users.noreply.github.com>
2023-05-11 18:52:44 +0100

    Add unit tests for GetNewsListUseCase and Flow

---
+++ app/src/test/java/mob/nereek/compose/FlowTest.kt
@@ -0,0 +1,227 @@
+package mob.nereek.compose
+
+import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import kotlin.system.measureTimeMillis
+
+class FlowTest {
+
+    @Test
+    fun testMutableStateFlowDuplicateValuesAreFilteredOut() = runBlocking {
+        // Déclaration d'un MutableStateFlow avec une valeur initiale de 0
+        val mutableCounter = MutableStateFlow(0)
+
+        // Liste pour stocker les valeurs collectées
+        val collectedValues = mutableListOf<Int>()
+
+        var i: Int = 0
+
+        val job = launch {
+            // Collecte les valeurs du MutableStateFlow dans collectedValues
+            mutableCounter.collect { value ->
+                collectedValues.add(value)
+                i++
+            }
+        }
+
+        delay(100)
+        mutableCounter.value = 1
+        delay(100)
+        mutableCounter.value = 2
+        delay(100)
+        mutableCounter.value = 2
+        delay(100)
+        mutableCounter.value = 3
+        delay(100)
+        mutableCounter.value = 3
+        delay(100)
+        mutableCounter.value = 4
+
+        delay(1000)
+        job.cancel()
+
+        delay(1000)
+        for (value in collectedValues) {
+            println("Value: $value")
+        }
+
+        delay(1000)
+        assertEquals(listOf(0, 1, 2, 3, 4), collectedValues)
+    }
+
+    @Test
+    fun testFlowWithDuplicates() = runBlocking {
+        // Liste mutable pour stocker les valeurs collectées
+        val mutableList = mutableListOf<Int>()
+
+        // Liste de listes pour stocker les valeurs collectées avec des doublons regroupés
+        val flowList = mutableListOf<List<Int>>()
+
+        // Définition d'un Flow émettant des valeurs
+        val flow: Flow<Int> = flow {
+            delay(100)
+            emit(1)
+            delay(100)
+            emit(2)
+            delay(100)
+            emit(2)
+            delay(100)
+            emit(3)
+            delay(100)
+            emit(3)
+            delay(100)
+            emit(4)
+        }
+
+        // Collecte les valeurs du Flow
+        flow.collect {
+            mutableList.add(it)
+
+            val lastFlowList = flowList.lastOrNull()
+            if (lastFlowList == null || lastFlowList.last() != it) {
+                // Si la dernière liste de flowList est nulle ou la dernière valeur est différente de it, on ajoute une nouvelle liste
+                flowList.add(listOf(it))
+            } else {
+                // Sinon, on ajoute la valeur it à la dernière liste existante
+                val newList = lastFlowList.toMutableList()
+                newList.add(it)
+                flowList[flowList.lastIndex] = newList
+            }
+        }
+
+        delay(1000)
+        println(mutableList) // Affiche les valeurs collectées
+        println(flowList) // Affiche les listes de valeurs avec des doublons regroupés
+
+        // Vérifie les résultats attendus
+        assertEquals(listOf(1, 2, 2, 3, 3, 4), mutableList)
+        //assertEquals(listOf(listOf(1), listOf(2), listOf(2), listOf(3), listOf(3), listOf(4)), flowList)
+    }
+
+    @Test
+    fun testSharedFlow() = runBlocking {
+        // Création d'un MutableSharedFlow avec le type Int
+        val sharedFlow = MutableSharedFlow<Int>()
+
+        val job1 = launch {
+            // Collecte les valeurs du SharedFlow dans la première coroutine
+            sharedFlow.collect {
+                println("Job 1: $it")
+            }
+        }
+
+        val job2 = launch {
+            // Collecte les valeurs du SharedFlow dans la deuxième coroutine
+            sharedFlow.collect {
+                println("Job 2: $it")
+            }
+        }
+
+        // Émet une valeur de 1 dans le SharedFlow
+        sharedFlow.emit(1)
+        delay(1000)
+
+        // Émet une valeur de 2 dans le SharedFlow
+        sharedFlow.emit(2)
+        delay(1000)
+
+        // Annulation du premier job
+        job1.cancel()
+        delay(1000)
+
+        // Émet une valeur de 3 dans le SharedFlow
+        sharedFlow.emit(3)
+        delay(1000)
+
+        // Annulation du deuxième job
+        job2.cancel()
+        delay(1000)
+
+        // Émet une valeur de 4 dans le SharedFlow
+        sharedFlow.emit(4)
+        delay(1000)
+
+        delay(1000)
+
+        // Attendre un peu pour laisser le temps aux jobs d'être annulés avant de terminer le test
+    }
+
+
+    @Test
+    fun testFlow() = runBlocking {
+        val flow = flow {
+            emit("Value 1")
+            emit("Value 1")
+            emit("Value 3")
+        }
+
+        val result = mutableListOf<String>()
+        flow.collect {
+            result.add(it)
+        }
+
+        assertEquals(listOf("Value 1", "Value 1", "Value 3"), result)
+    }
+
+
+    @Test
+    fun testCoroutineScopes() = runBlocking {
+        println("Start")
+
+        var time = measureTimeMillis {
+            // Lancement d'une coroutine avec GlobalScope (utilisation déconseillée)
+            GlobalScope.launch(Dispatchers.IO) {
+                //delay(1000L)
+                println("Coroutine 1 finished")
+            }
+        }
+
+        println("Coroutine 1 End Time = $time Millis")
+
+        time = measureTimeMillis {
+            // Lancement d'une coroutine avec le scope du test (runBlocking)
+            launch(Dispatchers.IO) {
+                //delay(1000L)
+                println("Coroutine 2 finished")
+            }
+        }
+
+        println("Coroutine 2 End Time = $time Millis")
+
+        time = measureTimeMillis {
+            // Lancement d'une coroutine avec le scope du test (runBlocking)
+            launch {
+                //delay(1000L)
+                println("Coroutine 3 finished")
+            }
+        }
+
+        println("Coroutine 3 End Time = $time Millis")
+    }
+
+
+    @Test
+    fun testAsync() = runBlocking {
+        val deferred1 = async {
+            "Hello"
+        }
+        val deferred2 = async {
+            "World"
+        }
+        val combinedResult = "${deferred1.await()}, ${deferred2.await()}!"
+        println(combinedResult)
+    }
+
+
+}
---
+++ app/src/test/java/mob/nereek/compose/GetNewsListUseCaseTest.kt
@@ -0,0 +1,119 @@
+package mob.nereek.compose
+
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import mob.nereek.compose.common.Constants
+import mob.nereek.compose.common.Resource
+import mob.nereek.compose.data.model.news.ArticleDto
+import mob.nereek.compose.data.model.news.NewsResponse
+import mob.nereek.compose.data.model.news.Source
+import mob.nereek.compose.domain.model.Article
+import mob.nereek.compose.domain.repository.NewsRepository
+import mob.nereek.compose.domain.usecase.news.GetNewsListUseCase
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Response
+
+class GetNewsListUseCaseTest {
+
+    private lateinit var newsRepository: NewsRepository
+    private lateinit var getNewsListUseCase: GetNewsListUseCase
+    private lateinit var article: Article
+    private lateinit var articleDtoList: List<ArticleDto>
+    private lateinit var articleList: List<Article>
+
+    val newsResponseMock = mockk<Response<NewsResponse>>()
+
+    @Before
+    fun setup() {
+        // Initialisation des variables et des objets nécessaires au test
+        newsRepository = mockk()
+        getNewsListUseCase = GetNewsListUseCase(newsRepository)
+
+        // Classe modèle de la couche data
+        articleDtoList = listOf(
+            ArticleDto(
+                author = "John Doe",
+                content = "This is the content of article 1",
+                description = "Description of article 1",
+                publishedAt = "2023-05-12T10:00:00Z",
+                source = Source(name = "Example News", id = "1"),
+                title = "Article 1",
+                url = "https://www.example.com/article1",
+                urlToImage = "https://www.example.com/image1.jpg"
+            ),
+            ArticleDto(
+                author = "Jane Smith",
+                content = "This is the content of article 2",
+                description = "Description of article 2",
+                publishedAt = "2023-05-13T12:30:00Z",
+                source = Source(name = "Another News Outlet", id = "2"),
+                title = "Article 2",
+                url = "https://www.example.com/article2",
+                urlToImage = "https://www.example.com/image2.jpg"
+            ),
+        )
+
+        // Classe modèle de la couche domaine
+        articleList = listOf(
+            Article(
+                author = "John Doe",
+                content = "This is the content of article 1",
+                description = "Description of article 1",
+                publishedAt = "2023-05-12T10:00:00Z",
+                source = Source(name = "Example News", id = "1"),
+                title = "Article 1",
+                url = "https://www.example.com/article1",
+                urlToImage = "https://www.example.com/image1.jpg"
+            ),
+            Article(
+                author = "Jane Smith",
+                content = "This is the content of article 2",
+                description = "Description of article 2",
+                publishedAt = "2023-05-13T12:30:00Z",
+                source = Source(name = "Another News Outlet", id = "2"),
+                title = "Article 2",
+                url = "https://www.example.com/article2",
+                urlToImage = "https://www.example.com/image2.jpg"
+            ),
+        )
+    }
+
+    @Test
+    fun testGetNewsListUseCase() = runBlocking {
+        // Définition des paramètres du test
+        val token = Constants.API_NEWS_TOKEN
+        val country = "us"
+
+        // Configuration des comportements simulés pour les appels de méthodes
+        coEvery { newsResponseMock.isSuccessful } returns true
+        coEvery { runBlocking { newsResponseMock.body()?.articles } } returns articleDtoList
+        coEvery { newsRepository.getNews(token, country) } returns newsResponseMock
+
+        // Appel de la méthode à tester
+        val result: Flow<Resource<List<Article>>> = getNewsListUseCase.invoke(token, country)
+
+        // Collecte des résultats dans une liste mutable pour les vérifications ultérieures
+        val resource = mutableListOf<Resource<List<Article>>>()
+        launch {
+            result.collect {
+                resource.add(it)
+            }
+        }
+
+        delay(1000) // Attente d'une seconde pour permettre la collecte des résultats
+
+        // Vérification des résultats attendus
+        assertEquals(Resource.Loading, resource.first()) // Vérifier la ressource de chargement
+        assertEquals(Resource.Success(articleList), resource.last()) // Vérifier les données réussies
+    }
+
+}
+
+
